907b657a58f3d4699b8669f3972dc6a132afd834
ayushcher1308 <ayush.cherkhiya@jungleworks.com>
2020-01-04 01:13:48 +0530

    search page

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,27 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ToursService } from '../service/tours.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.css']
+  styleUrls: ['./search-result.component.scss']
 })
 export class SearchResultComponent implements OnInit {
 
   constructor(private tours: ToursService,
-    private route:ActivatedRoute) { }
+    private route:ActivatedRoute,
+    private router:Router) { }
     tourName;
+    destAbout;
+    totalTours;
+    toursInfo;
 
   ngOnInit() {
+    document.getElementById("popular").classList.add("activeFilter");
 
     this.route.queryParamMap.subscribe(queryParams => {
       this.tourName = queryParams.get("tourName");
     })
 
-    this.tours.getTours('wayzook/tours/findByName?name='+this.tourName).subscribe(Response=>{
+    this.tours.getTours('wayzook/tours/getTourbyDestName?name='+this.tourName).subscribe(Response=>{
       console.log(Response);
+      this.toursInfo = Response;
+      this.totalTours = Response.length;
+      this.destAbout = Response[0].destAbout;
+      document.getElementById("loading").style.display="none";
     })
+  }
+
+  goToTour(id) {
+    this.router.navigate(['tour'], {
+      queryParams: { 'id': id }
+    });
   }
 
 }
