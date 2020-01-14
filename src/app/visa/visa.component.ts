7d6a196b6fd449f20cc35b9461852639dd990790
ayushcher1308 <ayush.cherkhiya@jungleworks.com>
2020-01-14 18:53:17 +0530

    passport

--- src/app/visa/visa.component.ts
+++ src/app/visa/visa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToursService } from '../service/tours.service';
 
 @Component({
   selector: 'app-visa',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisaComponent implements OnInit {
 
-  constructor() { }
+  Enquiry;
+
+  constructor(private fb:FormBuilder,private tours:ToursService) { }
 
   ngOnInit() {
+    this.Enquiry = this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      contact:['',Validators.required],
+      name:['',Validators.required],
+      desc:['',Validators.required]
+    })
   }
+
+  enquire()
+  {
+    document.getElementById("loading").style.display="block";
+    let header = this.Enquiry.value;
+    this.tours.checkUser('wayzook/userenquiry/add',header).subscribe(Response=>{
+        console.log(Response);
+        document.getElementById("loading").style.display="none";
+        this.Enquiry.reset;
+        this.snackBar();
+    })
+  }
+
+  snackBar() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
+
 
 }
