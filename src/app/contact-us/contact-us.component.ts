510117277d04e4db2df6bfb1495b51b954980799
ayushcher1308 <ayush.cherkhiya@jungleworks.com>
2020-01-15 17:27:00 +0530

    nav change

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToursService } from '../service/tours.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private tours:ToursService,
+    private fb:FormBuilder) { }
+    enquiryForm;
 
   ngOnInit() {
+    this.enquiryForm = this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      contact:['',Validators.required],
+      fname:['',Validators.required],
+      lname:['',Validators.required],
+      desc:['',Validators.required]
+    })
   }
+
+  enquire()
+  {
+    document.getElementById("loading").style.display="block";
+    let f = this.enquiryForm.value;
+    console.log(this.enquiryForm.valid)
+    let header={
+      name:f.fname+f.lname,
+      contact:f.contact,
+      email:f.email,
+      desc:f.desc
+    };
+    this.tours.checkUser("wayzook/userenquiry/add",header).subscribe(Response=>
+      {
+        console.log(Response);
+        this.enquiryForm.reset();
+        document.getElementById("loading").style.display="none";
+        
+        this.snackBar();
+      })
+  }
+
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
+
+  snackBar() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
+
 
 }
